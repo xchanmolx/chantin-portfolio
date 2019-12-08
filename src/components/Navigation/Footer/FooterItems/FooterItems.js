5fa23ead88ff83013fc97a03078590fa9079cbf0
xchanmolx <cmanigos@gmail.com>
2019-12-08 23:46:29 +0800

    chanmerc first commit footer

--- src/components/Navigation/Footer/FooterItems/FooterItems.js
+++ src/components/Navigation/Footer/FooterItems/FooterItems.js
@@ -8,8 +8,8 @@ import { Icon } from 'antd';
 const footerItems = (props) => (
     <ul className={classes.FooterItems}>
         <FooterItem><a href="www.github.com"><Icon type="github" /></a></FooterItem>
-        <FooterItem><a href="www.github.com"><Icon type="facebook" /></a></FooterItem>
-        <FooterItem><a href="www.github.com"><Icon type="youtube" /></a></FooterItem>
+        <FooterItem><a href="www.facebook.com"><Icon type="facebook" /></a></FooterItem>
+        <FooterItem><a href="www.youtube.com"><Icon type="youtube" /></a></FooterItem>
     </ul>    
 );
 
