8eb337d1a620ef02bd42cdfa974676c03f0036f3
xchanmolx <cmanigos@gmail.com>
2019-12-11 16:16:51 +0800

    update final

--- src/containers/Project/Project.js
+++ src/containers/Project/Project.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 import classes from './Project.module.css';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
+import AbtikImg from '../../assets/images/abtik-coming-soon.png';
+import AloguinsanImg from '../../assets/images/aloguinsan-coming-soon.png';
+import BogoChapelImg from '../../assets/images/bogo-chapel-coming-soon.png';
+import ReactTooltip from 'react-tooltip';
 import { Random } from 'react-animated-text';
 import { Typography, Row, Col } from 'antd';
 
@@ -16,12 +20,7 @@ const TextRandom = () => (
     effectChange={2.2} />
 );
 
-const colStyled = {
-    display: 'inline-block',
-    padding: '10px'
-}
-
-class Project extends Component {    
+class Project extends Component {   
 
     render () {
 
@@ -29,18 +28,19 @@ class Project extends Component {
             <Auxiliary>                                    
                 <div className={classes.Project}>
                     <Title style={{color: "palevioletred"}}><TextRandom /></Title>                       
-                    <Row style={{textAlign: 'center'}}>
-                        <Col span={8} style={colStyled}>
-                            Test 1
+                    <Row style={{textAlign: 'center'}}>                        
+                        <Col span={8} className={classes.ImgSection}>
+                            <a href="/"><img src={AbtikImg} alt="coming soon" data-tip="ABTIK Rescue Website | Under Construction!" /></a>                            
                         </Col>
-                        <Col span={8} style={colStyled}>
-                            Test 2
+                        <Col span={8} className={classes.ImgSection}>
+                            <a href="/"><img src={AloguinsanImg} alt="coming soon" data-tip="LGU Aloguinsan Website | Under Construction!" /></a>                          
                         </Col>
-                        <Col span={8} style={colStyled}>
-                            Test 3
+                        <Col span={8} className={classes.ImgSection}>
+                            <a href="/"><img src={BogoChapelImg} alt="coming soon" data-tip="Bogo, Aloguinsan Chapel Website | Under Construction!" /></a>                          
                         </Col>
+                        <ReactTooltip />
                     </Row>
-                </div>                
+                </div>
             </Auxiliary>
         );
     }
