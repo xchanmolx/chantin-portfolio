bc15523dae9ffdc36e184ec413999d8f6b397b4d
xchanmolx <cmanigos@gmail.com>
2019-12-11 12:49:17 +0800

    update index app

--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers} from 'redux';
-import thunk from 'redux-thunk';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware, compose} from 'redux';
+// import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const rootReducer = combineReducers({
-    // burgerBuilder: burgerBuilderReducer,
-    // order: orderReducer,
-    // auth: authReducer
-});
+// const rootReducer = combineReducers({
+//     // burgerBuilder: burgerBuilderReducer,
+//     // order: orderReducer,
+//     // auth: authReducer
+// });
 
-const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
-));
+// const store = createStore(rootReducer, composeEnhancers(
+//     applyMiddleware(thunk)
+// ));
+
+ // <Provider store={store}>
+    //     <BrowserRouter>
+    //         <App />
+    //     </BrowserRouter>
+    // </Provider>
 
 const app = (   
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
